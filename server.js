9fb4f6bc5c012aff734b5433a7b5cde85d2f12d4
alagujoth <jothipushpa33@gmail.com>
2016-09-23 16:00:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.sendFile('article one req and server here');
+});
+
+app.get('/article2', function (req, res) {
+  res.sendFile('article one req and server here');
+});
+
+app.get('/article3', function (req, res) {
+  res.sendFile('article one req and server here');
+});
+
+
+app.get('/article4', function (req, res) {
+  res.sendFile('article one req and server here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
